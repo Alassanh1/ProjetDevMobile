dee7749675ca1525647ba0a7ceb21f21c5c47b66
AlHasanh <alassanediatta080@gmail.com>
2025-07-04 11:35:59 +0000

    modification CreeCompteActivity

--- app/src/main/java/com/uasz/edt/v2025/CreerCompteActivity.java
+++ app/src/main/java/com/uasz/edt/v2025/CreerCompteActivity.java
@@ -4,146 +4,183 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
+import android.widget.*;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
 import com.uasz.edt.v2025.model.Classe;
+import com.uasz.edt.v2025.model.Etudiant;
 import com.uasz.edt.v2025.model.modelAdapter.DialogListeClassesAdapter;
+import com.uasz.edt.v2025.model.utilitaire.Constantes;
 import com.uasz.edt.v2025.model.utilitaire.DataConverter;
 import com.uasz.edt.v2025.model.utilitaire.JsonToObjectConverter;
-import com.uasz.edt.v2025.model.webservices.*;
+import com.uasz.edt.v2025.model.webservices.OperationsGEDT;
+import com.uasz.edt.v2025.model.webservices.RetoursOperationsGEDT;
 
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.List;
 
 public class CreerCompteActivity extends AppCompatActivity implements View.OnClickListener {
-    /* *** Références des éléments graphiques de l'écran d'accueil dans l'activité *** */
-    private EditText mPrenomInput;
-    private EditText mNomInput;
-    private EditText mReferenceInput;
-    private EditText mDateNaissanceInput;
-    private EditText mClasseInput;
-    private EditText mEmailConnexionInput;
-    private EditText mMotDePassConnexionInput;
+
+    private EditText mPrenomInput, mNomInput, mReferenceInput, mDateNaissanceInput,
+            mClasseInput, mEmailConnexionInput, mMotDePassConnexionInput;
     private Button mCreerCompteButton;
     private ImageView mCreerCompteRetour;
     private RadioGroup mSexe;
-    private RadioButton mHomme;
-    private RadioButton mFemme;
+    private RadioButton mHomme, mFemme;
 
-    //************ Pour la date : calendrier ***********//
     private DatePickerDialog datePickerDialog;
+    private AwesomeValidation awesomeValidation;
 
-    List<Classe> classeList;
+    private List<Classe> classeList;
 
     private OperationsGEDT mOperationsGEDT;
     private RetoursOperationsGEDT mRetoursOperationsGEDT;
     private JsonToObjectConverter mJsonToObjectConverter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_creer_compte);
+
         mOperationsGEDT = new OperationsGEDT();
         mJsonToObjectConverter = new JsonToObjectConverter();
 
         initView();
-
-        //********** Date calendrier ************//
         setDateTimeField();
-        //********** Date calendrier ************//
+        addValidationToViews();
     }
 
     private void initView() {
-        mEmailConnexionInput = (EditText) findViewById(R.id.activity_creer_compte_emailInput);
-        mMotDePassConnexionInput = (EditText) findViewById(R.id.activity_creer_compte_motDePassInput);
-        mCreerCompteButton = (Button) findViewById(R.id.activity_creer_compte_Button);
-        mPrenomInput = (EditText) findViewById(R.id.activity_creer_compte_prenomInput);
-        mNomInput = (EditText) findViewById(R.id.activity_creer_compte_nomInput);
-        mDateNaissanceInput = (EditText) findViewById(R.id.activity_creer_compte_date_naissanceInput);
-        mReferenceInput = (EditText) findViewById(R.id.activity_creer_compte_referenceInput);
-        mClasseInput = (EditText) findViewById(R.id.activity_creer_compte_classeInput);
-        mSexe = (RadioGroup) findViewById(R.id.activity_creer_compte_sexe);
-        mHomme = (RadioButton) findViewById(R.id.activity_creer_compte_homme);
-        mFemme = (RadioButton) findViewById(R.id.activity_creer_compte_femme);
+        mEmailConnexionInput = findViewById(R.id.activity_creer_compte_emailInput);
+        mMotDePassConnexionInput = findViewById(R.id.activity_creer_compte_motDePassInput);
+        mCreerCompteButton = findViewById(R.id.activity_creer_compte_Button);
+        mPrenomInput = findViewById(R.id.activity_creer_compte_prenomInput);
+        mNomInput = findViewById(R.id.activity_creer_compte_nomInput);
+        mDateNaissanceInput = findViewById(R.id.activity_creer_compte_date_naissanceInput);
+        mReferenceInput = findViewById(R.id.activity_creer_compte_referenceInput);
+        mClasseInput = findViewById(R.id.activity_creer_compte_classeInput);
+        mSexe = findViewById(R.id.activity_creer_compte_sexe);
+        mHomme = findViewById(R.id.activity_creer_compte_homme);
+        mFemme = findViewById(R.id.activity_creer_compte_femme);
+        mCreerCompteRetour = findViewById(R.id.creer_compte_retour);
 
-        mCreerCompteRetour = (ImageView) findViewById(R.id.creer_compte_retour);
-
-        mCreerCompteButton.setOnClickListener((View.OnClickListener) this);
-        mClasseInput.setOnClickListener((View.OnClickListener) this);
-        mCreerCompteRetour.setOnClickListener((View.OnClickListener) this);
-        mDateNaissanceInput.setOnClickListener((View.OnClickListener) this);
-        /*mSexe.setOnCheckedChangeListener((RadioGroup.OnCheckedChangeListener) this);
-        mHomme.setOnCheckedChangeListener((CompoundButton.OnCheckedChangeListener) this);
-        mFemme.setOnCheckedChangeListener((CompoundButton.OnCheckedChangeListener) this);*/
+        mCreerCompteButton.setOnClickListener(this);
+        mClasseInput.setOnClickListener(this);
+        mDateNaissanceInput.setOnClickListener(this);
+        mCreerCompteRetour.setOnClickListener(this);
     }
 
     private void setDateTimeField() {
-        Calendar newCalendar = Calendar.getInstance();
-        //newCalendar.
-        datePickerDialog = new DatePickerDialog(this, R.style.DatePickerDialogTheme, (view, year, monthOfYear, dayOfMonth) -> {
-            Calendar newDate = Calendar.getInstance();
-            newDate.set(year, monthOfYear, dayOfMonth);
-            mDateNaissanceInput.setText(DataConverter.toString(newDate.getTime(),DataConverter.DateType.SHORT));
-        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        Calendar calendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(this, R.style.DatePickerDialogTheme, (view, year, month, day) -> {
+            Calendar selected = Calendar.getInstance();
+            selected.set(year, month, day);
+            mDateNaissanceInput.setText(DataConverter.toString(selected.getTime(), DataConverter.DateType.SHORT));
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
     }
 
-    //*********** Dialog des classes *******************//
-    public void afficherDialogListeClasses(Activity activity){
-        mRetoursOperationsGEDT  = mOperationsGEDT.recuperer_liste_classes(CreerCompteActivity.this);
+    private void addValidationToViews() {
+        awesomeValidation = new AwesomeValidation(ValidationStyle.COLORATION);
+        awesomeValidation.setColor(Color.RED);
+        awesomeValidation.addValidation(this, R.id.activity_creer_compte_prenomInput, RegexTemplate.NOT_EMPTY, R.string.erreur_prenom);
+        awesomeValidation.addValidation(this, R.id.activity_creer_compte_nomInput, RegexTemplate.NOT_EMPTY, R.string.erreur_nom);
+        awesomeValidation.addValidation(this, R.id.activity_creer_compte_referenceInput, RegexTemplate.NOT_EMPTY, R.string.erreur_reference);
+        awesomeValidation.addValidation(this, R.id.activity_creer_compte_date_naissanceInput, RegexTemplate.NOT_EMPTY, R.string.erreur_date_naissance);
+        awesomeValidation.addValidation(this, R.id.activity_creer_compte_classeInput, RegexTemplate.NOT_EMPTY, R.string.erreur_classe);
+        awesomeValidation.addValidation(this, R.id.activity_creer_compte_emailInput, Patterns.EMAIL_ADDRESS, R.string.erreur_email);
+        awesomeValidation.addValidation(this, R.id.activity_creer_compte_motDePassInput, Constantes.Regex.REGEX_MOTDEPASSE, R.string.erreur_password);
+    }
+
+    public void afficherDialogListeClasses(Activity activity) {
+        mRetoursOperationsGEDT = mOperationsGEDT.recuperer_liste_classes(CreerCompteActivity.this);
         classeList = mJsonToObjectConverter.liste_classes_converter(mRetoursOperationsGEDT.getDataAsArray());
 
-        final Dialog dialogListeClasses = new Dialog(activity);
-        // dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialogListeClasses.setCancelable(false);
-        dialogListeClasses.setContentView(R.layout.dialog_afficher_liste_classes);
+        Dialog dialog = new Dialog(activity);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.dialog_afficher_liste_classes);
 
-        Button cancelButton = dialogListeClasses.findViewById(R.id.dialog_liste_classes_cancelButton);
-        cancelButton.setOnClickListener(v -> dialogListeClasses.dismiss());
+        Button cancel = dialog.findViewById(R.id.dialog_liste_classes_cancelButton);
+        cancel.setOnClickListener(v -> dialog.dismiss());
 
-        ListView listClassesView = dialogListeClasses.findViewById(R.id.dialog_list_classesView);
+        ListView list = dialog.findViewById(R.id.dialog_list_classesView);
         DialogListeClassesAdapter adapter = new DialogListeClassesAdapter(this, classeList);
-        listClassesView.setAdapter(adapter);
+        list.setAdapter(adapter);
 
-        listClassesView.setOnItemClickListener((parent, view, position, id) -> {
-            Classe classe = mJsonToObjectConverter.liste_classes_converter(mRetoursOperationsGEDT.getDataAsArray()).get(position);
+        list.setOnItemClickListener((parent, view, position, id) -> {
+            Classe classe = classeList.get(position);
             mClasseInput.setText(classe.getNiveauClasse().getCode_niveau() + " - " + classe.getCodeClasse());
-            dialogListeClasses.dismiss();
+            dialog.dismiss();
         });
-        dialogListeClasses.show();
+
+        dialog.show();
+    }
+
+    private Etudiant recupererFormulaireEtInstancierEtudiant() {
+        Etudiant etudiant = new Etudiant();
+        etudiant.setNom(mNomInput.getText().toString());
+        etudiant.setPrenom(mPrenomInput.getText().toString());
+        etudiant.setReference(mReferenceInput.getText().toString());
+        try {
+            etudiant.setDateNaissance(DataConverter.toDate(mDateNaissanceInput.getText().toString(), DataConverter.DateType.SHORT));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        etudiant.setEmail(mEmailConnexionInput.getText().toString());
+        etudiant.setPassword(mMotDePassConnexionInput.getText().toString());
+
+        String[] split = mClasseInput.getText().toString().split(" - ");
+        if (split.length == 2) {
+            String niveau = split[0];
+            String code = split[1];
+            etudiant.setClasse(mJsonToObjectConverter.getClasseFromCodeAndNiveau(code, niveau, classeList));
+        }
+
+        int selectedId = mSexe.getCheckedRadioButtonId();
+        RadioButton selectedRadio = findViewById(selectedId);
+        etudiant.setSexe(selectedRadio.getText().toString());
+
+        return etudiant;
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == mClasseInput.getId()){
-            try {
-                if (mOperationsGEDT.serviceInternet.connexionDisponible(getApplicationContext())) {
-                    afficherDialogListeClasses(CreerCompteActivity.this);
-                }else{
-                    Toast.makeText(CreerCompteActivity.this,
-                            R.string.erreur_internet_off,
-                            Toast.LENGTH_LONG).show();
-                }
-            } catch (Exception e) {
-                System.out.println(e.toString());
-                Toast.makeText(CreerCompteActivity.this,
-                        e.toString(),
-                        Toast.LENGTH_LONG).show();
+        int id = v.getId();
+        if (id == R.id.activity_creer_compte_classeInput) {
+            if (mOperationsGEDT.serviceInternet.connexionDisponible(getApplicationContext())) {
+                afficherDialogListeClasses(this);
+            } else {
+                Toast.makeText(this, R.string.erreur_internet_off, Toast.LENGTH_LONG).show();
             }
-
+        } else if (id == R.id.activity_creer_compte_date_naissanceInput) {
+            datePickerDialog.show();
+        } else if (id == R.id.creer_compte_retour) {
+            startActivity(new Intent(this, MainActivity2.class));
+        } else if (id == R.id.activity_creer_compte_Button) {
+            if (awesomeValidation.validate()) {
+                if (mOperationsGEDT.serviceInternet.connexionDisponible(getApplicationContext())) {
+                    Etudiant etudiant = recupererFormulaireEtInstancierEtudiant();
+                    mRetoursOperationsGEDT = mOperationsGEDT.ajouterEtudiant(etudiant);
+                    if (mRetoursOperationsGEDT.getValeurRetourOperationsGEDT() == Constantes.ValeurRetourOperationsGEDT.VALEUR_CREATED) {
+                        Toast.makeText(this, "Compte créé avec succès", Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(this, MainActivity2.class));
+                    } else {
+                        Toast.makeText(this, "Erreur : le compte n'a pas pu être créé", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(this, R.string.erreur_internet_off, Toast.LENGTH_LONG).show();
+                }
+            }
         }
     }
 }
